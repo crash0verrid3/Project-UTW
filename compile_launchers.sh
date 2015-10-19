39a96bf50417c0868a28a392777fa10d459a1cf4
Sasha Kaidalov <sasha@sasha-kaidalov>
2015-10-18 19:51:40 -0500

    Added launchers for windows/linux

--- compile_launchers.sh
+++ compile_launchers.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "Compiling..." &&
 echo "<================>" &&
-g++ easy_launcher.cpp -o runme_linux &&
+g++ easy_launcher.cpp -o launcher_linux &&
 echo "<================>" &&
 i586-mingw32msvc-g++ -o launcher.exe easy_launcher.cpp &&
 echo "<================>" &&
-echo "Done." &&
+echo "Done - Success." &&
 exit 0
+echo "Failed."
 exit 1
